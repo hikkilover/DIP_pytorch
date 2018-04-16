a7271ebd653fe13a53f65f79e24cb4576e67137b
hanxuan <autohanxuan@163.com>
2018-04-16 19:36:17 +0800

    my_network_test2018.4.16-2

--- my_network.py
+++ my_network.py
@@ -1,68 +1,76 @@
 import torch
 import torch.nn as nn
-from collections import OrderedDict
+from net_util import hourglassNetSuperparams
 
-class hourglass_deconv_network(nn.Module):
-    def __init__(self):
-        super(hourglass_deconv_network, self).__init__()
-        ch_in = 3
-        ch_pre = ch_in
-        self.down_layers_kernels = OrderedDict([("d1", 8), ("d2", 16), ("d3", 32), ("d4", 64), ("d5", 128), ("mid", 256)])
-        self.down_kernels_size = 5
-        self.up_layers_kernels =  OrderedDict([("u5", 128), ("u4", 64), ("u3", 32), ("u2", 16), ("u1", 8)])
-        self.up_kernels_size = 4
-        self.skip_layers_kernels =  OrderedDict([("s5", 4), ("s4", 4), ("s3", 4), ("s2", 0), ("s1", 0)])
-        self.skip_kernels_size = 5
+def down_layer(ch_in, ch_out, kernel_size, stride, padding, mode='conv'):
+    if mode == 'conv':
+        return nn.Sequential(
+                    nn.Conv2d(ch_in, ch_out, kernel_size, stride, padding),
+                    nn.BatchNorm2d(ch_out),
+                    nn.ReLU())
+
+def up_layer(ch_in, ch_out, kernel_size, stride, padding, act = 'relu',mode='deconv'):
+    if (mode == 'deconv'):
+        if (act == 'relu'):
+            return nn.Sequential(nn.ConvTranspose2d(ch_in, ch_out, kernel_size, stride, padding),
+                   nn.BatchNorm2d(ch_out),
+                   nn.ReLU())
+        elif (act == 'sigmoid'):
+            return nn.Sequential(nn.ConvTranspose2d(ch_in, ch_out, kernel_size, stride, padding),
+                   nn.BatchNorm2d(ch_out),
+                   nn.Sigmoid())
+    
+def skip_layer(ch_in, ch_out, kernel_size, stride, padding, mode='conv'):
+    if mode == 'conv':
+        return nn.Sequential(
+                    nn.Conv2d(ch_in, ch_out, kernel_size, stride, padding),
+                    nn.BatchNorm2d(ch_out),
+                    nn.ReLU())
+
+class hourglass_network(nn.Module):
+    def __init__(self, json, ch_in=3):
+        super(hourglass_network, self).__init__()
+
+        self.NSP = hourglassNetSuperparams(json)
         # bulid down layers
-        for key , value in self.down_layers_kernels.items():
-            self._modules[key] = nn.Sequential(
-                nn.Conv2d(ch_pre, value, self.down_kernels_size, 2,  2),
-                nn.BatchNorm2d(value),
-                nn.ReLU())
+        ch_pre = ch_in
+        for key , value in self.NSP.down_layers_kernels.items():
+            self._modules[key] = down_layer(ch_pre, value, self.NSP.down_kernels_size, 2,  2)
             ch_pre = value
 
         # build up layers
-        for key, value in self.up_layers_kernels.items():
-            if self.skip_layers_kernels['s'+key[-1]] !=0:
-                value_without_skip = value - self.skip_layers_kernels['s'+key[-1]]
+        for key, value in self.NSP.up_layers_kernels.items():
+            if self.NSP.skip_layers_kernels['s'+key[-1]] !=0:
+                value_without_skip = value - self.NSP.skip_layers_kernels['s'+key[-1]]
             else:
                 value_without_skip = value
-            self._modules[key] = nn.Sequential(
-                nn.ConvTranspose2d(ch_pre, value_without_skip, self.up_kernels_size, 2, 1),
-                nn.BatchNorm2d(value_without_skip),
-                nn.ReLU())
+            self._modules[key] = up_layer(ch_pre, value_without_skip, self.NSP.up_kernels_size, 2,  1, act = 'relu', mode='deconv')
             ch_pre = value
 
         # build out layer
-        self.out = nn.Sequential(
-            nn.ConvTranspose2d(ch_pre, ch_in, self.up_kernels_size, 2, 1),
-            nn.BatchNorm2d(ch_in),
-            nn.Sigmoid())
+        self.out = up_layer(ch_pre, ch_in, self.NSP.up_kernels_size, 2, 1,  act = 'sigmoid', mode='deconv')
 
         # build skip layers
-        for key , value in self.skip_layers_kernels.items():
+        for key , value in self.NSP.skip_layers_kernels.items():
             if value != 0:
-                ch_pre = self.down_layers_kernels['d'+key[-1]]
-                self._modules[key] =  nn.Sequential(
-                    nn.Conv2d(ch_pre, value, self.skip_kernels_size, 1, 2),
-                    nn.BatchNorm2d(value),
-                    nn.ReLU())
+                ch_pre = self.NSP.down_layers_kernels['d'+key[-1]]
+                self._modules[key] = skip_layer(ch_pre, value, self.NSP.skip_kernels_size, 1, 2)
 
     def forward(self, input):
         feature = {'input' : input}
         pre_key = 'input'
-        for key in self.down_layers_kernels.keys():
+        for key in self.NSP.down_layers_kernels.keys():
             feature[key] = self._modules[key](feature[pre_key])
             pre_key = key
-        for key in self.skip_layers_kernels.keys():
-            if self.skip_layers_kernels[key] != 0:
+        for key in self.NSP.skip_layers_kernels.keys():
+            if self.NSP.skip_layers_kernels[key] != 0:
                 feature[key] = self._modules[key](feature['d'+key[-1]])
-        for key in self.up_layers_kernels.keys():
+        for key in self.NSP.up_layers_kernels.keys():
             out = self._modules[key](feature[pre_key])
-            if self.skip_layers_kernels['s'+key[-1]] != 0:
+            if self.NSP.skip_layers_kernels['s'+key[-1]] != 0:
                 feature[key] = torch.cat([out, feature['s'+key[-1]]], 1)
             else:
                 feature[key] = out
             pre_key = key
         out = self.out(feature[pre_key])
-        return out
+        return out 
